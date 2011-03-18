67af4d5d7ba194a6eb6552c5564b7b347ce380d7
Greg <greg.whiteley@gmail.com>
2011-03-18 23:30:23 +1100

    Changed to use -unsafe and non-deprecated ComType variants

--- cs_rot/rot.cs
+++ cs_rot/rot.cs
@@ -1,43 +1,52 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-
-// TODO - use ComTypes.IRunningObjectTablel et al,...
+using System.Runtime.InteropServices.ComTypes;
 
 namespace whitty.ROT
 {
     public class RunningObjectTable
     {
 	#region Interop imports
-	[DllImport("ole32.dll")]  
-	public static extern int GetRunningObjectTable(int reserved, out UCOMIRunningObjectTable prot); 
- 
-	[DllImport("ole32.dll")]  
-	public static extern int  CreateBindCtx(int reserved, out UCOMIBindCtx ppbc);
+	[DllImport("ole32.dll")]
+	public static extern int GetRunningObjectTable(int reserved, out IRunningObjectTable prot);
+
+	[DllImport("ole32.dll")]
+	public static extern int  CreateBindCtx(int reserved, out IBindCtx ppbc);
 	#endregion
 
 	public static List<string> GetRunningObjectTableNames()
 	{
-	    UCOMIRunningObjectTable runningObjectTable;   
-	    GetRunningObjectTable(0, out runningObjectTable);    
+	    IRunningObjectTable runningObjectTable;
+	    GetRunningObjectTable(0, out runningObjectTable);
 
-	    UCOMIEnumMoniker monikers;
+	    IEnumMoniker monikers;
 	    runningObjectTable.EnumRunning(out monikers);
 
-	    int count;
-	    UCOMIMoniker[] moniker = new UCOMIMoniker[1];
+	    IMoniker[] moniker = new IMoniker[1];
 	    List<string> names = new List<string>();
 
-	    monikers.Reset();          
-	    while (monikers.Next(1, moniker, out count) == 0)
-	    {     
-		UCOMIBindCtx context;
-		CreateBindCtx(0, out context);     
+	    monikers.Reset();
+
+	    int result = 0;
+	    while (result == 0)
+	    {
+		unsafe
+		{
+		    int count;
+		    System.IntPtr pCount = (System.IntPtr)(&count);
+		    result = monikers.Next(1, moniker, pCount);
+		}
+		if (result != 0)
+		    break;
+
+		IBindCtx context;
+		CreateBindCtx(0, out context);
 
 		string name;
 		moniker[0].GetDisplayName(context, null, out name);
 
 		names.Add(name);
-	    } 
+	    }
 	    return names;
 	}
     }
